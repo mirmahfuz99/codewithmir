90ae8a46a371f315365a165bb27d8c1de3499f92
Mir Mahfuz <mirmahfuz10@gmail.com>
2021-04-24 00:12:27 +0600

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Post
+
+def frontpage(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/frontpage.html', {'posts': posts})
